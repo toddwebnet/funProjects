a72dc1133832c6c3e6b1116dc95470ca162e07c1
James Todd <toddwebnet@gmail.com>
2018-07-29 17:37:13 -0500

    udpate

--- fileCopy/masCopy.php
+++ fileCopy/masCopy.php
@@ -7,26 +7,70 @@ use App\FileCopier;
 use App\SQLiteConnection;
 USE App\Config;
 
-$fileCopier = new FileCopier();
-$source = '/Volumes/Public/shoregroup';
-$target = '/Volumes/data/shoregroup';
+$copies = [
+    [
+        'source' => '/Volumes/Public/Sentry',
+        'target' => '/Volumes/data/Sentry',
+    ],
+    [
+        'source' => '/Volumes/Public/Reliance',
+        'target' => '/Volumes/data/Reliance',
+    ],
+    [
+        'source' => '/Volumes/Public/projects',
+        'target' => '/Volumes/data/projects'
+    ],
+    [
+        'source' => '/Volumes/Public/PrinterLogic',
+        'target' => '/Volumes/data/PrinterLogic',
+    ],
+    [
+        'source' => '/Volumes/Public/njt',
+        'target' => '/Volumes/data/njt',
+    ],
+    [
+        'source' => '/Volumes/Public/netbook',
+        'target' => '/Volumes/data/netbook',
+    ],
+    [
+        'source' => '/Volumes/Public/Movies',
+        'target' => '/Volumes/data/Movies',
+    ],
+    [
+        'source' => '/Volumes/Public/Media',
+        'target' => '/Volumes/data/Media',
+    ],
+];
+foreach ($copies as $block) {
+    $source = $block['source'];
+    $target = $block['target'];
 
-if (!file_exists($source)) {
-    print("path " . $source . "does exit\n\n");
-    die();
+    if (!file_exists($target)) {
+        mkdir($target);
+    }
+    goCopy($source, $target);
 }
+function goCopy($source, $target)
+{
+    $fileCopier = new FileCopier();
 
-if (!file_exists($target)) {
-    print("path " . $target . " does exit\n\n");
-    die();
-}
-$fileCopier->flushCommands();
-print "collecting files \n";
+    if (!file_exists($source)) {
+        print("path " . $source . "does exit\n\n");
+        die();
+    }
+
+    if (!file_exists($target)) {
+        print("path " . $target . " does exit\n\n");
+        die();
+    }
+    $fileCopier->flushCommands();
+    print "collecting files \n";
 //$fileCopier->collectFiles(Config::SOURCE_PATH, Config::TARGET_PATH);
 
-$fileCopier->processSourcePath($source, $target);
-print "copying files\n";
-$fileCopier->processCommands();
+    $fileCopier->processSourcePath($source, $target);
+    print "copying files\n";
+    $fileCopier->processCommands();
 
-print "done.\n\n";
+    print "done.\n\n";
+}
 
